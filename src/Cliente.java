0f0a98583c5ec381eb7d3e0f130632b889f45b7c
omar9610 <omar.gadelrabali01@universitadipavia.it>
2019-03-06 23:09:22 +0100

    "Classe Cliente"

---
+++ src/Cliente.java
@@ -0,0 +1,53 @@
+
+public class Cliente {
+
+    private String id;
+    private String username;
+    private String password;
+    private String telefono;
+    private String indirizzo;
+    private String email;
+    private String nome;
+    private String cognome;
+    private String sesso;
+    private String dataNascita;
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getTelefono() { return telefono; }
+
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getIndirizzo() { return indirizzo; }
+
+    public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCognome() { return cognome; }
+
+    public void setCognome(String cognome) { this.cognome = cognome; }
+
+    public String getSesso() { return sesso; }
+
+    public void setSesso(String sesso) { this.sesso = sesso; }
+    public String getDataNascita() { return dataNascita; }
+
+    public void setDataNascita(String dataNascita) { this.dataNascita = dataNascita; }
+}
