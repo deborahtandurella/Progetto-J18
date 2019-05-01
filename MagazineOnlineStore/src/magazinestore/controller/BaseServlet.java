c1aceacc2c95fb17cb52a7ced37b79898ad6679c
Alaa Eldin <alaaeldin.abdelmohsen01@universitadipavia.it>
2019-05-01 15:43:00 +0200

    Progetto rifatto Versione 1.0

--- MagazineOnlineStore/src/magazinestore/controller/BaseServlet.java
+++ MagazineOnlineStore/src/magazinestore/controller/BaseServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+/*BaseServlet*/
 public class BaseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
